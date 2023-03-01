41a2d112c2a4fb29c7e38b726ef5411c85419cf4
Roan Carpenter <roan.carpenter@mrjaffesclass.com>
2023-03-01 14:15:06 -0800

    Just got it monthly

--- StockPriceHistory.java
+++ StockPriceHistory.java
@@ -1,6 +1,23 @@
 /**
  * Retrieve historical stock prices
  */
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import yahoofinance.histquotes.HistQuotesRequest;
+import yahoofinance.histquotes.Interval;
+import yahoofinance.quotes.fx.FxQuote;
+import yahoofinance.quotes.csv.FxQuotesRequest;
+import yahoofinance.quotes.csv.StockQuotesData;
+import yahoofinance.quotes.csv.StockQuotesRequest;
+import yahoofinance.quotes.query1v7.FxQuotesQuery1V7Request;
+import yahoofinance.quotes.query1v7.StockQuotesQuery1V7Request;
+
+import yahoofinance.*;
+  import java.util.Calendar;
+
 public class StockPriceHistory
 {
 
@@ -13,9 +30,25 @@ public class StockPriceHistory
         try {
             Stock stock = YahooFinance.get(TICKER, true);
             System.out.println(stock);
+            System.out.println(stock.getHistory());
         } catch (Exception e) {
             System.out.println("Error in stock call");    
         }
+    }
+    
+    
+    /**
+     * Sends a request with the historical quotes included
+     * at the specified interval (DAILY, WEEKLY, MONTHLY).
+     * Returns null if the data can't be retrieved from Yahoo Finance.
+     * 
+     * @param symbol        the symbol of the stock for which you want to retrieve information
+     * @param interval      the interval of the included historical data
+     * @return              a {@link Stock} object containing the requested information
+     * @throws java.io.IOException when there's a connection problem
+     */
+    public static Stock get(String symbol, Interval interval) throws IOException {
+        return YahooFinance.get(symbol, HistQuotesRequest.DEFAULT_FROM, HistQuotesRequest.DEFAULT_TO, interval);
     }
     
     /**
